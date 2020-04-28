6e878c491cffaae952b1c4082a541239e0247dc9
AguPereyra <apereyra404@alumnos.iua.edu.ar>
2020-04-28 18:56:11 -0300

    TP4: Cache Implementada.

--- src/tp04/classes/webglrenderer.js
+++ src/tp04/classes/webglrenderer.js
@@ -3,13 +3,19 @@ const twgl = require('twgl.js')
 class WebglRenderer {
   constructor (canvas) {
     this._gl = twgl.getWebGLContext(canvas)
+    this._cache = {
+      figures: []
+    }
   }
 
   render (scene, camera) {
     const vs = require('./shaders/vertex-shader.glsl')
     const fs = require('./shaders/fragment-shader.glsl')
-    const programInfo = twgl.createProgramInfo(this._gl, [vs, fs])
-    this._gl.useProgram(programInfo.program)
+    // Cache de programa
+    if (!this._cache.programInfo) {
+      this._cache.programInfo = twgl.createProgramInfo(this._gl, [vs, fs])
+    }
+    this._gl.useProgram(this._cache.programInfo.program)
 
     twgl.resizeCanvasToDisplaySize(this._gl.canvas)
     this._gl.clearColor(0.2, 0.2, 0.2, 1)
@@ -22,21 +28,24 @@ class WebglRenderer {
     for (i; i < scene._meshes.length; i++) {
       let mesh = scene._meshes[i]
 
-      const arrays = {
-        aPosition: mesh._geometry._vertices,
-        aColor: mesh._material,
-        indices: mesh._geometry._faces
+      //  Cache de bufferInfo
+      if (!this._cache.figures[i]) {
+        const arrays = {
+          aPosition: mesh._geometry._vertices,
+          aColor: mesh._material,
+          indices: mesh._geometry._faces
+        }
+        this._cache.figures[i] = twgl.createBufferInfoFromArrays(this._gl, arrays)
       }
-      const bufferInfo = twgl.createBufferInfoFromArrays(this._gl, arrays)
 
       const uniforms = {
         uProjectionMatrix: camera.projectionMatrix,
         uViewMatrix: camera.viewMatrix,
         uModelMatrix: mesh.modelMatrix
       }
-      twgl.setBuffersAndAttributes(this._gl, programInfo, bufferInfo)
-      twgl.setUniforms(programInfo, uniforms)
-      twgl.drawBufferInfo(this._gl, bufferInfo,
+      twgl.setBuffersAndAttributes(this._gl, this._cache.programInfo, this._cache.figures[i])
+      twgl.setUniforms(this._cache.programInfo, uniforms)
+      twgl.drawBufferInfo(this._gl, this._cache.figures[i],
         (mesh._drawAsTriangle) ? this._gl.TRIANGLES : this._gl.LINES)
     }
   }
